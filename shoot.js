fce16b80b3b021d834ad85f37131948d64e7ccb0
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2023-03-16 21:39:47 +0530

    Add files via upload

--- shoot.js
+++ shoot.js
@@ -1,6 +1,9 @@
 AFRAME.registerComponent("bowling-balls", { 
   init: function () { 
-    this.throwBall(); }, 
+   
+    this.throwBall(); 
+  
+  }, 
     throwBall: function () { 
       window.addEventListener("keydown", (e) => { 
         if (e.key === "z") 
@@ -9,11 +12,18 @@ AFRAME.registerComponent("bowling-balls", {
          ball.setAttribute("gltf-model", "ball"); 
          ball.setAttribute("scale", { x: 3, y: 3, z: 3}); 
          var cam = document.querySelector("#camera"); 
-         pos = cam.getAttribute("position"); 
+        var pos = cam.getAttribute("position"); 
          ball.setAttribute("position", { x: pos.x, y: pos.y-1.2, z: pos.z, }); 
          var camera = document.querySelector("#camera").object3D; 
           var direction = new THREE.Vector3(); 
           camera.getWorldDirection(direction); 
           ball.setAttribute("velocity", direction.multiplyScalar(-10)); 
           var scene = document.querySelector("#scene"); 
-          scene.appendChild(ball); } }); }, });
+          scene.appendChild(ball); } 
+        
+        }); 
+      
+      }, 
+    
+    }
+    );
